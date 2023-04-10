8f7ff6846976fd8043a04f469789680a2e7cc2d5
Renata Yermak <renatkahturo@gmail.com>
2023-04-10 18:59:36 +0300

    .

--- src/main/java/com/epam/mjc/nio/FileReader.java
+++ src/main/java/com/epam/mjc/nio/FileReader.java
@@ -1,11 +1,38 @@
 package com.epam.mjc.nio;
 
+import java.io.BufferedReader;
 import java.io.File;
-
+import java.io.IOException;
+import java.nio.file.Files;
 
 public class FileReader {
 
     public Profile getDataFromFile(File file) {
-        return new Profile();
+        Profile profile = new Profile();
+
+        try (BufferedReader reader = Files.newBufferedReader(file.toPath())) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                String key = line.split(" ")[0];
+                String value = line.split(" ")[1];
+                switch (key) {
+                    case "Name:":
+                        profile.setName(value);
+                        break;
+                    case "Age:":
+                        profile.setAge(Integer.parseInt(value));
+                        break;
+                    case "Email:":
+                        profile.setEmail(value);
+                        break;
+                    case "Phone:":
+                        profile.setPhone(Long.parseLong(value));
+                        break;
+                }
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return profile;
     }
 }
